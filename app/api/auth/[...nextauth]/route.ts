4826176d7ebbaa192178c86d9bdcbe17c473e571
Perspicacious <johnayomide920@gmail.com>
2024-10-27 20:04:40 +0100

    ALL IS WELL

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,65 +1,28 @@
-// import { AuthOptions } from "next-auth";
-// import NextAuth from "next-auth";
-// import GithubProvider from "next-auth/providers/github";
-// import GoogleProvider from "next-auth/providers/google";
-// import { PrismaAdapter } from "@next-auth/prisma-adapter"
-
-// import prisma from "@/lib/prismadb";
-
-// export const authOptions: AuthOptions = {
-//   adapter: PrismaAdapter(prisma),
-// providers: [
-//   GoogleProvider({
-//     clientId: process.env.GOOGLE_CLIENT_ID as string,
-//     clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-//   }),
-//   GithubProvider({
-//     clientId: process.env.GITHUB_CLIENT_ID as string,
-//     clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-//   })
-// ],
-//     pages:{
-//         signIn: '/sign-in',
-//     },
-//     secret: process.env.NEXTAUTH_SECRET,
-// };
-
-// const handler = NextAuth(authOptions);
-
-// export { handler as GET, handler as POST };
-
-
-// route.ts
-import { NextApiRequest, NextApiResponse } from 'next';
-import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import NextAuth, { AuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import prisma from "@/lib/prismadb";
 
-export const authOptions = {  // Export authOptions
+export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-    }),
   ],
-  pages:{
-    signIn: '/sign-in',
+  pages: {
+    signIn: "/sign-in",
   },
   secret: process.env.NEXTAUTH_SECRET,
 };
 
-export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
-  return NextAuth(req, res, authOptions);
-};
+const handler = NextAuth(authOptions);
 
-export const POST = async (req: NextApiRequest, res: NextApiResponse) => {
-  return NextAuth(req, res, authOptions);
-};
+export { handler as GET, handler as POST };
