9a95a9e853dcb73b548ef71328c370987ba61c8c
Perspicacious <johnayomide920@gmail.com>
2024-10-30 14:19:20 +0100

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -21,8 +21,7 @@ const nextConfig = {
     ],
   },
   env: {
-    NEXTAUTH_URL: 'http://localhost:3000',
-    // https://hubpost2.vercel.app
+    NEXTAUTH_URL: 'https://hubpost2.vercel.app',
     CLOUDINARY_UPLOAD_PRESET: 'bgtdnw0r',
   },
 };
