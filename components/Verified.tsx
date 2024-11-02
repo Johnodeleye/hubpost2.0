4c373d9c89619aa81e9376c910278bffa6a97ed7
Perspicacious <johnayomide920@gmail.com>
2024-11-02 13:02:47 +0100

    Update Verified.tsx

--- components/Verified.tsx
+++ components/Verified.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import Image from 'next/image';
 import { files } from '@/app/assets/files';
 
-const verifiedUserIds = ['67110a882ef002fb20e97156', '67110f1c2ef002fb20e9715a', '672412b819b4e5b906e5a01d','672415b949fb23ee4fb70739']; 
+const verifiedUserIds = ['67110a882ef002fb20e97156', 
+                         '67110f1c2ef002fb20e9715a', 
+                         '672412b819b4e5b906e5a01d',
+                         '672415b949fb23ee4fb70739',
+                         '67243985e7c3836a041065f9'
+                        ]; 
 
 interface VerifiedProps {
   authorId: string;
